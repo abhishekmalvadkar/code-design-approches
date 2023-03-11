bd69f2f09414492cc1a0e9f95d16ef605fd93fd5
abhishek malvadkar ranoji <abhishek@dev.com>
2023-03-11 17:03:02 +0530

    Created Spring Boot Application

---
+++ code-design-approches/src/main/java/in/code/design/CodeDesignApprochesApplication.java
@@ -0,0 +1,13 @@
+package in.code.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDesignApprochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDesignApprochesApplication.class, args);
+	}
+
+}
