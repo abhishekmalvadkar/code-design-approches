9d1572e2bc8e4eb7f182eae75d6eab9795665208
abhishek malvadkar ranoji <abhishek@dev.com>
2023-03-11 18:20:03 +0530

    Design four

---
+++ code-design-approches/src/main/java/in/code/design/DesignFour.java
@@ -0,0 +1,32 @@
+package in.code.design;
+
+public class DesignFour {
+	
+	/*
+	 * You should give boolean methods or varibales names meaningfully like
+	 * - canUse
+	 * - hasRole
+	 * - isPublic
+	 * - etc....
+	 */
+	
+	public static void doSomething(String content) {
+		if (content == null) {
+			return; // if content is null then I don't want to throw any exception , I just want to return control so below code will not
+			        // exceute if content is null
+		}
+		
+		System.out.println("Data processing....");
+	}
+	
+	public static void doSomeThingWithClassType(Class<?> classType) { // This is how we can take class type as method parameter 
+		                                                             // ? means any type
+		System.out.println(classType);
+	}
+	
+	public static void main(String[] args) {
+		doSomeThingWithClassType(String.class); // This is how we can pass class type as method parameter
+		doSomeThingWithClassType(DesignOne.class);
+	}
+
+}
